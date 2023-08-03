5c25ba4b42d434f2fb8e470ef0977f5783d4c6af
vladikshk <vladikshkwok@gmail.com>
2023-08-03 12:30:41 +0300

    Added rabbitMQ for notification service

--- notification/src/main/java/ru/vladikshk/notification/NotificationApplication.java
+++ notification/src/main/java/ru/vladikshk/notification/NotificationApplication.java
@@ -1,12 +1,20 @@
 package ru.vladikshk.notification;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import ru.vladikshk.amqp.RabbitMQMessageProducer;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "ru.vladikshk.notification",
+                "ru.vladikshk.amqp",
+        }
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "ru.vladikshk.clients"
@@ -15,4 +23,18 @@ public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig
+//    ) {
+//        return args -> {
+//            producer.publish(new Person("Vlad", 23),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String name, int age){}
 }
